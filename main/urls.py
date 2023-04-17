ec1bd6b3a63d6c636588c2f2881f3a5fef4134ff
Ursa <77912443+Phyo-Alpha@users.noreply.github.com>
2023-04-17 10:40:41 +0800

    Cinema Booking System template (Login / Register)

---
+++ main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from . import views
+
+urlpatterns = [
+    path("", views.mainPage, name="mainPage"),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+]
