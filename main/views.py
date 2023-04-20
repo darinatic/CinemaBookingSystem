425447a4bf146d8cee865676b9786d5c2912a685
Ursa <77912443+Phyo-Alpha@users.noreply.github.com>
2023-04-20 18:58:37 +0800

    added the add movie button

--- main/views.py
+++ main/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render, redirect
+from .models import Movie
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def mainPage(response):
+   
     if not response.user.is_authenticated:
         return redirect ("/login")
+    
+    if response.POST.get("addMovie"):
+        m = Movie()
+        m.save()
+        movies = Movie.objects.all()
+        return render(response, "CinemaCustomerPages/home.html", {"movies": movies})
         
     return render(response, "CinemaCustomerPages/home.html")
