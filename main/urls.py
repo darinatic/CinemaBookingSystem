0877a4417d9c230dd4a5c794f041566ba92b128d
Phyo-Alpha <77912443+Phyo-Alpha@users.noreply.github.com>
2023-05-05 14:57:27 +0800

    Replaced the method to POST instead of GET in movie_details and Cart

--- main/urls.py
+++ main/urls.py
@@ -6,10 +6,13 @@ from . import views
 urlpatterns = [
     path("", views.mainPage, name="mainPage"),
     path("home/", views.mainPage, name="home"),
+    path("homealter/", views.mainPageAlter, name="homealter"),
     path("movie_details/<int:movie_id>", views.movie_details, name="movie_details"),
+    path("test/<int:session_id>", views.test, name="test"),
     path('addtoCart/', views.addtoCart, name='addtoCart'),
     path('ticketcart/', views.ticketcart, name='ticketcart'),
     path('updateCart/', views.updateCart, name='updateCart'),
     path('checkout/', views.checkout, name='checkout'),
+    path('checkOutCart/', views.checkOutCart, name='checkOutCart'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse
 from .models import Movie, MovieSession, CinemaRoom, Ticket
 from django.forms.models import model_to_dict
 from django.views.decorators.csrf import csrf_exempt
@@ -29,7 +29,7 @@ def mainPage(response):
             movie_session = MovieSession (movie_id= movie, room_id = CinemaRoom.objects.get(room_id = 1), start_time = datetime.now())
             movie_session.save()
     
-    sessions = MovieSession.objects.select_related('movie_id').all().values('movie_id__movie_id', 'movie_id__movie_title', 'movie_id__movie_duration', 'movie_id__movie_genre','movie_id__movie_img', 'movie_id__movie_description' ,'movie_id__is_active', 'room_id__room_id', 'room_id__room_name', 'start_time')
+    sessions = MovieSession.objects.select_related('movie_id').all().values('session_id','movie_id__movie_id', 'movie_id__movie_title', 'movie_id__movie_duration', 'movie_id__movie_genre','movie_id__movie_img', 'movie_id__movie_description' ,'movie_id__is_active', 'room_id__room_id', 'room_id__room_name', 'start_time')
     sessions_list = list(sessions)
     
     for session in sessions_list:
@@ -44,15 +44,16 @@ def movie_details(response, movie_id):
     return render(response, 'CinemaCustomerPages/movie_detail.html', {'movie': movie,'movie_json': movie_json})
 
 def addtoCart(request):
-    data = request.GET.get("data")
-    request.session["cart"] = data
-    return redirect("/ticketcart")
+    if request.method == "POST":
+        json_data = json.loads(request.body)
+        data = json_data.get("data")
+        request.session["cart"] = data
+        return HttpResponse("OK")
     
 def ticketcart(request):
     data = request.session.get("cart")
-    tickets = json.loads(data) if data else [] 
+    tickets = json.loads(json.dumps(data)) if data else [] 
     context = {"tickets": json.dumps(tickets)}
-    
     return render(request, "CinemaCustomerPages/ticketcart.html", context)
 
 @csrf_exempt
@@ -64,12 +65,30 @@ def updateCart(request):
         return redirect("/ticketcart")
     
 def checkout(request):
-    data = request.GET.get("data")
-    tickets = json.loads(data) if data else []
-    return render(request, "CinemaCustomerPages/myPurchasedTickets.html", {"tickets" : json.dumps(tickets)})
+    if request.method == "POST":
+        json_data = json.loads(request.body)
+        data = json_data.get("data")
+        request.session["checkout"] = data
+        return HttpResponse("OK")
+        
+def checkOutCart(request):
+    data = request.session.get("checkout")
+    tickets = json.loads(json.dumps(data)) if data else []
+    context = {"tickets": json.dumps(tickets)}
+    return render(request, "CinemaCustomerPages/myPurchasedTickets.html", context)
 
 def purchaseTickets(request):
     pass
 
+def test(request, session_id):
+    session = MovieSession.objects.get(session_id=session_id)
+    movie = session.movie_id
+    print(movie)
+    session.movie = movie
+    session.start_time = session.start_time.strftime("%Y-%m-%d %H:%M:%S")
+    session_json = json.dumps(model_to_dict(session))
+    return render(request, 'CinemaCustomerPages/test.html', {'session': session,'session_json': session_json})
 
 
+def mainPageAlter(request):
+    return render (request, 'CinemaCustomerPages/homealternative.html')
